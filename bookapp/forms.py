eca4317ff857a9540be60dc550d5c54a48c4ed19
yook11 <pv4qjwdfg2@privaterelay.appleid.com>
2025-12-30 13:25:26 +0900

    feat: タイトルと著者に関するカスタムバリデーションの実装とエラー表示の改善

--- bookapp/forms.py
+++ bookapp/forms.py
@@ -17,3 +17,23 @@ class BookForm(forms.ModelForm):
         widgets = {
             'publication_date': forms.DateInput(attrs={'type': 'date'})
         }
+    
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if '盗作' in title:
+            raise forms.ValidationError(
+                "タイトルに盗作を含めることはできません。"
+            )
+        return title
+    
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get('title')
+        author = cleaned_data.get('author')
+
+        if title == author:
+            raise forms.ValidationError(
+                'タイトルと著者名は同じにはできません。'
+            )
+        return cleaned_data

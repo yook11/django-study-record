9e7cd6faac368831352a542ce94b91f5f9c4af43
yook11 <pv4qjwdfg2@privaterelay.appleid.com>
2026-01-02 12:47:01 +0900

    feat: ToDoの作成・詳細・更新機能を実装

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+# todoapp/urls.py
 urlpatterns = [
-    path('list/', views.TodoListView.as_view(), name='todo_list')
+    # ① まず一覧を表示する（ここにアクセスしてボタンを押す）
+    path('list/', views.TodoListView.as_view(), name='todo_list'),
+
+    # ② ボタンを押した後に飛んでくる詳細ページ
+    path('<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
+
+    path('new/', views.TodoCreateView.as_view(), name='todo_create'),
+
+    path('<int:pk>/edit/', views.TodoupdateView.as_view(), name='todo_update')
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from . import models
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView,)
+from django.utils.timezone import localtime
+from django.urls import reverse_lazy
 
 class TodoListView(ListView):
     model = models.Todo
     template_name = 'todoapp/todo_list.html'
     context_object_name = 'todos'
 
-# Create your views here.
+class TodoDetailView(DetailView):
+    model = models.Todo
+    template_name = 'todoapp/todo_detail.html'
+    context_object_name = 'todo'
+
+class TodoCreateView(CreateView):
+    model = models.Todo
+    template_name = 'todoapp/todo_create.html'
+    fields = ['title', 'memo', 'completed']
+    success_url = reverse_lazy('todo_list')
+
+
+class TodoupdateView(UpdateView):
+    model = models.Todo
+    template_name = 'todoapp/todo_update.html'
+    fields = ['title', 'memo', 'completed']
+    success_url = reverse_lazy('todo_list')
+
+    def form_vaild(self, form):
+        todo = form.save()
+        print(f"タイトル: '{todo.title}' 更新時間: {localtime(todo.updated)}")
+        return super().form_vaild(form)
+

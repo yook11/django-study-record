5cee3e4d4a33ec67f461f6c56c98e557d3382d44
yook11 <pv4qjwdfg2@privaterelay.appleid.com>
2026-01-11 16:03:05 +0900

    メニューページにログイン必須機能を追加

--- myproject/views.py
+++ myproject/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class MenuPageView(TemplateView):
+# ==================================================
+# メニュー画面用のビュー：継承元はTemplateView + LoginRequiredMixin
+# ==================================================
 
+class MenuPageView(LoginRequiredMixin,TemplateView):
+    # 使用するテンプレートを指定
     template_name = 'menu.html'
